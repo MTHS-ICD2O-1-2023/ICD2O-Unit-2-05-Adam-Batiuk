8dceb66819cbcb37e2cf08313934dd46d09f5d91
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-25 18:10:19 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+'use strict'
+
+function myButtonClicked() {
+  const workHours = parseInt(document.getElementById('work-hours').value)
+  const payRate = parseInt(document.getElementById("pay-rate").value)
+
+  const takeHomeSalary = (payRate * workHours) * (1 - 0.18)
+  const moneyTaxed = (payRate * workHours) * 0.18
+
+  document.getElementById('answer').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+  document.getElementById('answer-two').innerHTML = 'The government gets: $' + moneyTaxed.toFixed(2)
+}
