9f8a696414c18d76fbe96003c378f50233b7ad92
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-25 18:12:37 +0000

    format

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@
 // Created by: Adam Batiuk
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
-'use strict'
+"use strict"
 
 function myButtonClicked() {
-  const workHours = parseInt(document.getElementById('work-hours').value)
+  const workHours = parseInt(document.getElementById("work-hours").value)
   const payRate = parseInt(document.getElementById("pay-rate").value)
 
-  const takeHomeSalary = (payRate * workHours) * (1 - 0.18)
-  const moneyTaxed = (payRate * workHours) * 0.18
+  const takeHomeSalary = payRate * workHours * (1 - 0.18)
+  const moneyTaxed = payRate * workHours * 0.18
 
-  document.getElementById('answer').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
-  document.getElementById('answer-two').innerHTML = 'The government gets: $' + moneyTaxed.toFixed(2)
+  document.getElementById("answer").innerHTML =
+    "Your pay will be: $" + takeHomeSalary.toFixed(2)
+  document.getElementById("answer-two").innerHTML =
+    "The government gets: $" + moneyTaxed.toFixed(2)
 }
